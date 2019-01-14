2dee284b29d374613b586a859c8ed421706151a1
shanpu <cheztomo513@gmail.com>
2019-01-14 13:48:26 +0900

    Fix arch

--- main.go
+++ main.go
@@ -3,11 +3,9 @@ package main
 import (
 	"github.com/StudioAquatan/slack-invite-bot/handler"
 	"github.com/StudioAquatan/slack-invite-bot/model"
-	"github.com/StudioAquatan/slack-invite-bot/service"
 	"log"
 	"net/http"
 
-	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/labstack/echo"
 
@@ -16,9 +14,6 @@ import (
 )
 
 type envConfig struct {
-	// Port is server port to be listened.
-	Port string `envconfig:"PORT" default:"3000"`
-
 	// BotToken is bot user token to access to slack API.
 	SlackBotToken string `envconfig:"SLACK_BOT_TOKEN" required:"true"`
 
@@ -32,88 +27,74 @@ type envConfig struct {
 	// Bot responses to the mention in this channel.
 	SlackChannelID string `envconfig:"SLACK_CHANNEL_ID" required:"true"`
 
-	//Esa Wiki Invitation URL
-	WikiInvitationUrl string `envconfig:"WIKI_URL" required:"true" default:"wiki_url"`
-
 	//Trello Invitation URL
-	TrelloInvitationUrl string `envconfig:"TRELLO_URL" required:"true" default:"trello_url"`
+	//TrelloInvitationUrl string `envconfig:"TRELLO_URL" required:"true" default:"trello_url"`
 }
 
 func main() {
-	// Initialize DB
-	InitDB()
-
 	e := echo.New()
 
 	// Routing
+	// Hello World!
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.POST("/new/member/", registerMember)
-	e.POST("/new/slack/", informMemberSlack)
+
+	//google formからのPOSTを受けてslackに承認を投げる
+	e.POST("/slack/", postSlack)
+
+	//slackからの返信を受け取る
+	e.POST("/interactive/", interactionSlack)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":8000"))
 }
 
-func InitDB() {
-	db, err := gorm.Open("sqlite3", "db.sqlite3")
-	if err != nil {
-		log.Printf("[ERROR] Invalid action was submitted: %s", err)
-		panic("failed to connect database")
-
-	}
-	defer db.Close()
-
-	// Migrate the schema
-	db.AutoMigrate(&model.Member{})
-}
-
-func registerMember(c echo.Context) (err error) {
-	db, err := gorm.Open("sqlite3", "db.sqlite3")
-	if err != nil {
-		log.Printf("[ERROR] Invalid action was submitted: %s", err)
-		panic("failed to connect database")
-	}
-	defer db.Close()
-
-	m := new(model.Member)
-	if err = c.Bind(m); err != nil {
-		return
+func postSlack(c echo.Context) (err error) {
+	// TODO ここわざわざmemberモデル生成しなくてもいい?
+	post := new(model.Member)
+	if err := c.Bind(post); err != nil {
+		return err
 	}
 
-	service.CreateMember(db, *m)
-	return c.JSON(http.StatusOK, m)
-}
-
-func informMemberSlack(c echo.Context) (err error) {
 	var env envConfig
 	if err := envconfig.Process("", &env); err != nil {
 		log.Printf("[ERROR] Failed to process env var: %s", err)
 		return err
 	}
 
-	// Listening slack event and response
-	log.Printf("[INFO] Start slack event listening")
 	client := slack.New(env.SlackBotToken)
-	slackListener := &handler.SlackListener{
-		client:    client,
-		botID:     env.SlackBotID,
-		channelID: env.SlackChannelID,
+	slackBotInfo := &handler.SlackBotInfo{
+		Client:    client,
+		BotID:     env.SlackBotID,
+		ChannelID: env.SlackChannelID,
 	}
-	go slackListener.ListenAndResponse()
 
-	// Register handler to receive interactive message
-	// responses from slack (kicked by user action)
-	http.Handle("/slack/interaction", handler.interactionHandler{
-		verificationToken: env.SlackVerificationToken,
-	})
+	// mailアドレスも一緒におくる
+	if err := slackBotInfo.PostMessageEvent(post.Email); err != nil {
+		log.Printf("[ERROR] Failed to post message: %s", err)
+	}
 
-	log.Printf("[INFO] Server listening on :%s", env.Port)
-	if err := http.ListenAndServe(":"+env.Port, nil); err != nil {
-		log.Printf("[ERROR] %s", err)
+	return c.String(http.StatusOK, "[INFO] Complete send Message to Slack")
+}
+
+func interactionSlack(c echo.Context) (err error) {
+	post := new(slack.InteractionCallback)
+	if err := c.Bind(post); err != nil { //TODO c.Bind(post)してもpostに値が格納されない
 		return err
 	}
 
-	return c.String(http.StatusOK, "[INFO] Complete send Message to Slack") //todo もう少しマシなメッセージを…
+	var env envConfig
+	if err := envconfig.Process("", &env); err != nil {
+		log.Printf("[ERROR] Failed to process env var: %s", err)
+		return err
+	}
+
+	client := slack.New(env.SlackBotToken)
+	interactionSlack := &handler.InteractionSlack{
+		SlackClient:       client,
+		VerificationToken: env.SlackVerificationToken,
+	}
+
+	return interactionSlack.ServeInteractiveSlack(c, post)
 }
